cd6abdf29b501c205c9e624cc5e9a7791980f06d
BattleDex <143239271+BattleDemon@users.noreply.github.com>
2025-08-28 14:35:35 +1000

    Made Basic Structure

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print ("hello world")
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import QUrl, Qt
+from PyQt6.uic import loadUi
+import os
+
+import sys
+
+app = QApplication(sys.argv)
+
+class JournalApp(QMainWindow):
+    def __init__(self):
+        super(JournalApp, self).__init__()
+
+
+
+        self.show()
+
+
+
+main = JournalApp
+
+sys.exit(app.exec) 
